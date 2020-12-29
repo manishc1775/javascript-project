0613d8975b0503f1e2912fe5e31cc336db41fff8
manishc1775 <manishchafle@gmail.com>
2020-12-29 11:54:26 +0530

    functions and parameters

---
+++ assets/scripts/app.js
@@ -0,0 +1,14 @@
+const defaultResult=0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('the result is' +result);
+}
+add(1, 1);
+add(5, 5);
+
+currentResults= (currentResult + 10) * 3 / 2 - 1;
+let calculationDescription= `(${defaultResult} + s 10) * 3 / 2-1`;
+
+outputResult(currentResult ,calculationDescription,);
